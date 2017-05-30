13acb794cc46f75fac3d268c71275e2fae0cff8d
Kike <kike@areavisual.net>
2017-05-30 16:19:11 +0200

    Incluyo campos nuevos para activación
    Introduzco cambios para hacer activación en el alta del registro.

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $password="root";
-$db="myapp";
+$db="usuariosregistrados";
 $con = new mysqli($host,$user,$password,$db);
 
 ?>
--- registro.sql
+++ registro.sql
@@ -1,11 +1,42 @@
-create database myapp;
-use myapp;
+CREATE DATABASE  IF NOT EXISTS `usuariosregistrados` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `usuariosregistrados`;
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: localhost    Database: myapp
+-- ------------------------------------------------------
+-- Server version	5.6.28
 
-create table user(
-	id int not null auto_increment primary key,
-	fullname varchar(500) not null,
-	username varchar(100) not null unique,
-	email varchar(255) not null unique,
-	password varchar(255) not null,
-	created_at datetime not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(500) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `activation_key` int(32) NOT NULL,
+  `validated` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
