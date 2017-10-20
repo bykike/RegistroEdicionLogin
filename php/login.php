52dd4ac9a02f9b78ce6ffb264914e5a42d1c0c8c
Kike <kike@areavisual.net>
2017-10-20 16:54:10 +0200

    Comprobación de la validación
    Para acceder se comprueba que la validación se activara confirmando por
    medio del correo.

--- php/login.php
+++ php/login.php
@@ -4,25 +4,25 @@ if(!empty($_POST)){
 	if(isset($_POST["username"]) &&isset($_POST["password"])){
 		if($_POST["username"]!=""&&$_POST["password"]!=""){
 			include "conexion.php";
-			
+
 			$user_id=null;
-			$sql1= "select * from user where (username=\"$_POST[username]\" or email=\"$_POST[username]\") and password=\"$_POST[password]\" ";
+			$sql1= "select * from user where (username=\"$_POST[username]\" or email=\"$_POST[username]\")
+							and password=\"$_POST[password]\"
+							and validated='1'";
 			$query = $con->query($sql1);
 			while ($r=$query->fetch_array()) {
 				$user_id=$r["id"];
 				break;
 			}
 			if($user_id==null){
-				print "<script>alert(\"Acceso invalido.\");window.location='../login.php';</script>";
+				print "<script>alert(\"Acceso invalido. Asugúrese de haber introducido correctamente los datos y tener su cuenta validada.\");window.location='../login.php';</script>";
 			}else{
 				session_start();
 				$_SESSION["user_id"]=$user_id;
-				print "<script>window.location='../home.php';</script>";				
+				print "<script>window.location='../home.php';</script>";
 			}
 		}
 	}
 }
-
-
 
 ?>
