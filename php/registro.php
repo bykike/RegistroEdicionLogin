776d0379b7e2ec43fce23a87dfb073c639a54610
Kike <kike@areavisual.net>
2017-11-01 10:09:14 +0100

    Registro, envío de correo para activación de cuenta
    Comprueba que no existe el correo, registra y envía correo para su
    activación.

--- php/registro.php
+++ php/registro.php
@@ -4,7 +4,7 @@
 		if(isset($_POST["username"]) &&isset($_POST["fullname"]) &&isset($_POST["email"]) &&isset($_POST["password"]) &&isset($_POST["confirm_password"])){
 			if($_POST["username"]!=""&& $_POST["fullname"]!=""&&$_POST["email"]!=""&&$_POST["password"]!=""&&$_POST["password"]==$_POST["confirm_password"]){
 				include "conexion.php";
-				
+
 				$found=false;
 				$sql1= "select * from user where username=\"$_POST[username]\" or email=\"$_POST[email]\"";
 				$query = $con->query($sql1);
@@ -15,23 +15,25 @@
 				if($found){
 					print "<script>alert(\"Nombre de usuario o email ya estan registrados.\");window.location='../registro.php';</script>";
 				}
-				// Género un randomke
+				// Genero un randomke
 				$random_key = generate_random_key();
 				$validated = 0;
 				// Meto contenido en la base de datos
-				$sql = "insert into user(username,fullname,email,password,created_at, activation_key, validated) value (\"$_POST[username]\",\"$_POST[fullname]\",\"$_POST[email]\",\"$_POST[password]\",NOW(),\"$random_key\",\"$validated\")";
+				$sql = "insert into user(username, fullname, email, password, created_at, activation_key, validated)
+							value (\"$_POST[username]\",\"$_POST[fullname]\",\"$_POST[email]\",\"$_POST[password]\",NOW(),\"$random_key\",\"$validated\")";
 				$query = $con->query($sql);
 				if($query!=null){
-					print "<script>alert(\"Registro exitoso. Proceda a logearse\");window.location='../login.php';</script>";
+					print "<script>alert(\"Registro exitoso. Proceda a logearse una vez que active la cuenta desde el correo electrónico que recibirá. Asegúrese ver la bandeja de entrada o bien la carpeta de spam.\");window.location='../login.php';</script>";
 
 				$email = $_POST["email"];
+				$headers = "From: name@domain.com" . "\r\n" . "CC: name@domain.com";
 				// Enviar un mensaje para confirmar la dirección de email introducida
-				 mail($email,"Activación de la cuenta",
+				mail($email,"Activación de la cuenta",
 				     "Gracias por registrarse en nuestro sitio.
 				     Su cuenta ha sido creada, y debe ser activada antes de poder ser utilizada.
 				     Para activar la cuenta, haga click en el siguiente enlace o copielo en la
 				     barra de direcciones del navegador:
-				     http://activate.php?activation=".$random_key);
+				     http://www.domain.com/RegistroEdicionLogin/activate.php?activation=".$random_key, $headers);
 
 
 				}
@@ -42,7 +44,7 @@
 
 function generate_random_key() {
     $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
- 
+
     $new_key = "";
     for ($i = 0; $i < 32; $i++) {
         $new_key .= $chars[rand(0,35)];
